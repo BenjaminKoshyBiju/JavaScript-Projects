b72c8d9c4c0453d257e1fa19876a96c711cc5d5b
BenjaminKoshyBiju <bennn455@gmail.com>
2023-03-04 08:01:23 +0530

    created apis get post

--- Mern/server/controllers/todoController.js
+++ Mern/server/controllers/todoController.js
@@ -1,7 +1,47 @@
-class todoController{
-    static getalltodos=async(req,res)=>{
+import todoModel from "../models/todoModel.js";
+class todoController {
+  static getalltodos = async (req, res) => {
+    try {
+      const fetchalltodos = await todoModel.find({});
+      return res.status(200).json(fetchalltodos);
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  };
+  static addNewtodo = async (req, res) => {
+    const { title, isCompleted } = req.body;
+    try {
+      if (title && isCompleted) {
+        const addtodo = new todoModel({
+          title,
+          isCompleted,
+        });
+        const savedtodo = await addtodo.save();
+        return res.status(200).json({ message: "Success" });
+      } else {
+        return res.status(400).json({ message: "Fill both fields!" });
+      }
+    } catch (error) {}
+    return res.status(400).json({ message: error.message });
+  };
 
-    };
-   
+  static getnewtodo= async(req,res)=>{
+    const {id}=req.params;
+    try{
+        if(id)
+        {
+            const fetch1data=await todoModel.findById(id);
+            return res.status(200).json({fetch1data });
+        }
+        else{
+            return res.status(400).json({message: "invalid id" });
+        }
+
+    }
+    catch(error)
+    {
+        return res.status(400).json({ message: error.message });
+    }
+  }
 }
-export default todoController
+export default todoController;
--- Mern/server/routes/todoRoute.js
+++ Mern/server/routes/todoRoute.js
@@ -2,4 +2,6 @@ import express from "express";
 import todoController from "../controllers/todoController.js";
 const router=express.Router();
 router.get("/todos",todoController.getalltodos);
+router.post("/todos",todoController.addNewtodo);
+router.get("/todos",todoController.getnewtodo);
 export default router;
